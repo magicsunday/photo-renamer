4e38393dc8c42dc574069eb235c1879aae555d95
Rico Sonntag <mail@ricosonntag.de>
2025-09-02 09:05:23 +0200

    Update application class

--- src/Application.php
+++ src/Application.php
@@ -23,8 +23,32 @@ use Symfony\Component\Console\Command\Command;
  */
 class Application extends \Symfony\Component\Console\Application
 {
+    /**
+     * The name of the application.
+     *
+     * @var string
+     */
     private const string NAME = 'renamer';
 
+    /**
+     * The path to the version file.
+     *
+     * @var string
+     */
+    private const string VERSION_FILE = __DIR__ . '/../version';
+
+    /**
+     * The default version if the version file is not available.
+     *
+     * @var string
+     */
+    private const string DEFAULT_VERSION = '0.0.0';
+
+    /**
+     * The logo.
+     *
+     * @var string
+     */
     private static string $logo = ' .____                         .__                                .___               ____.
  |   _|   _____ _____     ____ |__| ____   ________ __  ____    __| _/____  ___.__. |_   |
  |  |    /     \\\\__  \   / ___\|  |/ ___\ /  ___/  |  \/    \  / __ |\__  \\\<   |  |   |  |
@@ -35,27 +59,43 @@ class Application extends \Symfony\Component\Console\Application
 ';
 
     /**
+     * Constructor.
+     *
      * @param iterable<Command> $commands
      */
     public function __construct(iterable $commands)
     {
-        $version = file_get_contents(__DIR__ . '/../version');
-
-        if ($version === false) {
-            $version = '0.0.0';
-        }
-
         parent::__construct(
             self::NAME,
-            trim(
-                $version,
-                PHP_EOL
-            )
+            $this->loadVersion()
         );
 
         foreach ($commands as $command) {
             $this->add($command);
         }
+    }
+
+    /**
+     * Loads the version from the specified version file if it exists.
+     * If the file does not exist or cannot be read, returns the default version.
+     *
+     * @return string the loaded version or the default version if loading fails
+     */
+    private function loadVersion(): string
+    {
+        $content = false;
+
+        if (file_exists(self::VERSION_FILE)) {
+            $content = file_get_contents(self::VERSION_FILE);
+        }
+
+        if ($content === false) {
+            return self::DEFAULT_VERSION;
+        }
+
+        $version = trim($content, PHP_EOL);
+
+        return $version !== '' ? $version : self::DEFAULT_VERSION;
     }
 
     #[Override]
