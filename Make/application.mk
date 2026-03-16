2bb4c85e2ccf2ef5ef6d849d3441f3cb5643f2af
Rico Sonntag <mail@ricosonntag.de>
2026-03-16 13:32:47 +0100

    refactor: move build scripts to scripts/, make .build cache-only

--- Make/application.mk
+++ Make/application.mk
@@ -7,16 +7,16 @@
 .PHONY: build cleanup init init-with-docker version
 
 build: .logo ## Build a new renamer binary.
-	@bash .build/build
+	@bash scripts/build
 
 cleanup: .logo ## Removes build artifacts to free space.
 	@rm -rf spc/pkgroot/ spc/downloads/ spc/source/
 
 init: .logo ## Initialize the SPC build environment.
-	@bash .build/init
+	@bash scripts/init
 
 init-with-docker: .logo ## Initialize the SPC build environment with Docker.
-	@bash .build/init-with-docker
+	@bash scripts/init-with-docker
 
 version: .logo ## Create a new version release.
 	@bash scripts/create-version
