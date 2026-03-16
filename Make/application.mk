4de0750c787e4ddc39d233e57c5cc322df4bdd0f
Rico Sonntag <mail@ricosonntag.de>
2026-03-16 14:56:09 +0100

    refactor: always build binary via Docker

--- Make/application.mk
+++ Make/application.mk
@@ -4,12 +4,9 @@
 
 #### Application
 
-.PHONY: binary binary-docker binary-clean version
+.PHONY: binary binary-clean version
 
 binary: .logo ## Build the self-contained renamer binary.
-	@bash scripts/build
-
-binary-docker: .logo ## Build the binary using Docker (no local toolchain needed).
 	@bash scripts/init-with-docker
 	@bash scripts/build
 
