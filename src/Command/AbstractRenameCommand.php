33c131c8609a3315e2d61b425b100a1238839ebf
Rico Sonntag <mail@ricosonntag.de>
2026-03-17 11:01:05 +0100

    refactor: add use function assert import in AbstractRenameCommand

--- src/Command/AbstractRenameCommand.php
+++ src/Command/AbstractRenameCommand.php
@@ -29,6 +29,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
+use function assert;
 use function getcwd;
 use function is_string;
 use function ltrim;
