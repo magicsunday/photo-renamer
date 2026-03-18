dac42906e33c46dbbfe82f8eb1adffddc432c9e3
Rico Sonntag <mail@ricosonntag.de>
2026-03-18 18:18:37 +0100

    fix: strip scan root directory from output paths

--- src/Service/FileSystemService.php
+++ src/Service/FileSystemService.php
@@ -26,7 +26,6 @@ use RuntimeException;
 use SplFileInfo;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
-use function basename;
 use function copy;
 use function file_exists;
 use function in_array;
@@ -395,14 +394,7 @@ class FileSystemService implements FileSystemServiceInterface
         $prefix = $normalizedBase . DIRECTORY_SEPARATOR;
 
         if (str_starts_with($pathname, $prefix)) {
-            $relativePath = substr($pathname, strlen($prefix));
-            $baseName     = basename($normalizedBase);
-
-            if (($baseName === '') || ($baseName === DIRECTORY_SEPARATOR)) {
-                return $relativePath;
-            }
-
-            return $baseName . DIRECTORY_SEPARATOR . $relativePath;
+            return substr($pathname, strlen($prefix));
         }
 
         return $pathname;
